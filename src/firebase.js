e375525fda2534c685a1f67bb9a7787cc7e747fd
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-09-09 18:12:01 -0700

    se creo el proyecto

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzPzCWLeqJI4df6Q8IRx33nhfTIDxGUAw",
+    authDomain: "facebook-clone-f13d2.firebaseapp.com",
+    databaseURL: "https://facebook-clone-f13d2.firebaseio.com",
+    projectId: "facebook-clone-f13d2",
+    storageBucket: "facebook-clone-f13d2.appspot.com",
+    messagingSenderId: "37598128224",
+    appId: "1:37598128224:web:10c05eaa75e03924597b05",
+    measurementId: "G-8Y1GR44N4T"
+  };
