4de7a78c2391141667a2b60b0b754b4fbc5a25b0
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-09-16 11:49:39 -0700

    Se termino feed y post y se agrego widget

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from "./Header";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
+import Widget from "./Widget";
 function App() {
   return (
     <div className="app">
@@ -11,10 +12,9 @@ function App() {
       <Header />
       <div className="app__body">
       <SideBar />
-        {/* FEED */}
         <Feed />
         {/* WIDGETS */}
-        
+        <Widget />
       </div>
      </div>
   );
--- src/Feed.js
+++ src/Feed.js
@@ -11,9 +11,27 @@ function Feed(){
         <StoryReel />
         <MessageSender />
 
-        <Post />        
-        <Post />        
-        <Post />        
+        <Post 
+        profilePic="https://cdn.psychologytoday.com/sites/default/files/styles/image-article_inline_full/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=ji6Xj8tv"
+        username="Alexander"
+        timestamp="time"
+        message="imagen"
+        image="https://code.org/shared/images/social-media/codeorg2019_social.png"
+        />        
+        <Post
+        profilePic=""
+        message=""
+        timestamp=""
+        username=""
+        image=""
+        />        
+        <Post 
+        profilePic=""
+        message=""
+        timestamp=""
+        username=""
+        image=""
+        />        
         </div>
     )
 }
